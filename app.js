b275f4fa30dee498f0d2d0a0f52b32d2a6a78fc3
ritik4314 <ritikkamboj4314@gmail.com>
2021-09-17 16:56:19 +0530

    JS: input , output with event listener

--- app.js
+++ app.js
@@ -6,16 +6,33 @@
 // console.log(`teh lo bhai aish karo ${x}`);
 
 var btnTranslate = document.querySelector("#btn-translate");
-console.log(btnTranslate);
+// console.log(btnTranslate);
 
 
 
-const input=document.querySelector("#txt-input");
-console.log(d);
+var input=document.querySelector("#txt-input");
+
+// console.log(d);
+var output= document.querySelector("#txt-output");
+
 function jai()
 {
-    console.log("jai baabe ki");
-    console.log("yeh pahad tod rakhe hai tumne :" + input.value );
+    // console.log("jai baabe ki");
+    // console.log("yeh pahad tod rakhe hai tumne :" + input.value );
+    // console.log()
+    output.innerHTML="pee popo poooo laa :" + input.value;
+    // console.log(output.innerHTML);
 }
 
 btnTranslate.addEventListener("click",  jai);
+
+// console.log(output.innerHTML);
+
+// const output2=document.querySelector("#txt-output");
+// console.log(output2);
+// console.log(output2.value); // kyoki box mein hamne kuch enter hi nahi kara 
+// console.log(output2.innerHTML);// yeh output dega kyoki html mien toh kara rakha hai 
+
+// output2.innerHTML="aaja bete yash jaldi time kharab ho raha hai ,era ";
+// console.log(output2.innerHTML);
+// console.log(output2.value);
