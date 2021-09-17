206b1fbbdf3f8cab605077114ab254d27ac90490
ritik4314 <ritikkamboj4314@gmail.com>
2021-09-17 12:51:52 +0530

    JS: getting textInput onto console

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-console.log("jai maat di ");
+// // console.log("jai maat di ");
 
-alert("this script work ");
- const x =prompt("give me your amazon working ID ");
-console.log(x);
+// // alert("this script work ");
+//  let x =prompt("give me your amazon working ID ");
+// // console.log(x);
+// console.log(`teh lo bhai aish karo ${x}`);
+
+var btnTranslate = document.querySelector("#btn-translate");
+console.log(btnTranslate);
+
+
+
+const input=document.querySelector("#txt-input");
+console.log(d);
+function jai()
+{
+    console.log("jai baabe ki");
+    console.log("yeh pahad tod rakhe hai tumne :" + input.value );
+}
+
+btnTranslate.addEventListener("click",  jai);
